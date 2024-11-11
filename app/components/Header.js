26f6c8b1b768a8c218d4ad42d0a83760f951629a
Lololosa4ka <ilyakatereniuk@gmail.com>
2024-11-11 14:57:38 +0700

    add basic layout

---
+++ app/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1 className='center'>Discover hidden places in the world around you</h1>
+    </div>
+  )
+}
+
+export default Header
