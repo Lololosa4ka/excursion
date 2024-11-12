ab0cf1edd2441a21a03bd5f89ee3672a8c967a8d
Lololosa4ka <ilyakatereniuk@gmail.com>
2024-11-12 14:11:42 +0700

    completed version

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import Link from 'next/link'
+import Img from 'next/image'
 const Main = () => {
   return (
-    <div>Main</div>
+    <div>
+        <div className='center'><Link href="/"><div className='link'>Download Excursion (Coming soon)</div></Link></div>
+
+            <div className="center">
+                <video autoPlay={true} muted="" loop="" >
+                <source src="https://content.codecademy.com/programs/freelance-one/excursion/videos/excursion.mp4" type="video/mp4" />
+                </video>
+            </div>
+        <div className="center"><h2>Your personal travel guide</h2>
+        <h4>Excursion remembers places you like, and recommends new points of interest around you.</h4>
+        </div>
+        <div className="center"><img src="https://content.codecademy.com/programs/freelance-one/excursion/images/camp.jpg"></img></div>
+        <div className="center"><img src="https://content.codecademy.com/programs/freelance-one/excursion/images/phone.png"></img></div>
+        <div className="center">
+            <h2>Coming Soon for iPhone and Android</h2>
+        </div>
+        <div className="center">
+            <Link href="https://4c5e24dcab6045e0981fe0978bc08696.cc-propeller.cloud/?allowEmbedForCodecademy=false&allowCopyAndPasteForCodecademy=true&allowFileUploadForCodecademy=true&allowFileDownloadForCodecademy=true&allowExternalLinkForCodecademy=true&allowAICodeExplainForCodecademy=true&allowAIChatAssistantForCodecademy=true&allowAIErrorExplainForCodecademy=true#"><div className='link'>Download Excursion (Coming soon)</div></Link>
+        </div>
+        <div className="footer">
+            <h4 className='align-right'>© Excursion</h4>
+        </div>
+    </div>
   )
 }
 
--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
 import react from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
-import Footer from "./components/Footer";
 export default function page() {
     return (
     <div>
         <div>
         <Header></Header>
         <Main></Main>
-        <Footer></Footer>
     </div>
     </div>
     )
